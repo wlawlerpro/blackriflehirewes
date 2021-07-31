ed0e4ba51a87ef2547689b51aadceb93b8b1228e
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2021-07-31 14:35:25 -0400

    started installing routing and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { MeComponent } from './mefolder/me/me.component';
+import { SkillComponent } from './skillfolder/skill/skill.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: AppComponent, pathMatch: 'full'},
+  {path: 'me', component: MeComponent},
+  {path: 'skill', component:SkillComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
