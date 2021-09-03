e6715bae8d5a6b3b7c58c06d7bdc9acfd20efee8
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2021-09-03 15:23:49 -0400

    put button in that will disapper when routing to the me section of the site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { OutletContext, RouterOutlet, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+ // animation: [ ''],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
   title = 'Welcome to Black Rifle Hire Wes';
+  mainMenu = true;
+
+  constructor(private router: Router) {}
+/*
+prepareRoute(outlet: RouterOutlet) {
+  return outlet && OutletContext.activatedRouteData && OutletContext.activatedRouteData['animation'];
+
+}*/
+
+ngOnInit () {
+
+}
+
+menuToggle() {
+  this.mainMenu = !this.mainMenu;
+  this.router.navigate(['./me']);
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeComponent } from './mefolder/me/me.component';
@@ -14,6 +14,7 @@ import { SkillComponent } from './skillfolder/skill/skill.component';
   ],
   imports: [
     BrowserModule,
+   // BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
