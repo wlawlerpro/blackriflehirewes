216b3e4a957343f19102600a7dabf59bb10aeb7c
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2022-11-01 19:46:17 -0400

    added demo storage serve file mockend

--- src/app/skillfolder/product.ts
+++ src/app/skillfolder/product.ts
@@ -14,12 +14,12 @@ export interface Product {
 	    price: 69,
 	    productImage: 'https://lh5.googleusercontent.com/wtDyETV2XZBpRjpExK4oLAIoYcCP6AUTGL7Id0dycl8v3oFwaLR9Ei0FDqcxiVFdy2A=w2400',
 	    preview: 'Be you Be Andy',
-	    description: ` Are you feeling lonely? Are you sad? Fear not because coming soon to your desk is
+	    description: ` Are you feeling lonely? Are you sad? Fear not, because coming soon to your desk is
 	    your own autograph picture of the Black Rifle Coffee CEO, Andy Stumpf.
 	     Aimed at brightening your day, this stallion of a coffee roaster and wine sommelier, 
 	     will warm your heart as gaze longingly into his eyes. For the low low price of $69.69,
 	      you too can be on your way to stop being such a bitch. There’s no time to waste, 
-	      get your piece of this dick drawing davinci today. `
+	      get your piece of this dick drawing davinci today! `
 
 	    
 	}, 
@@ -33,7 +33,7 @@ export interface Product {
 	{   id:3,
 	    name: "Starship Troopers: The Logan Stark Reboot", 
 	    price: 20, 
-	    productImage: 'https://lh5.googleusercontent.com/cQuN_0s8QOYYa9mx43OMO_Cdc-KpfCHrKEPGAIs6brsuV43jhAeuRzRmjdVX8vGqntQ=w2400',
+	    productImage: 'https://lh4.googleusercontent.com/u-cT25TQi2tXKzAloPPxWi-bMkDjDlZFhB0OhwPo_qssY1cPpx9zXQWLKm8a3AQAeQQ=w2400',
 	    preview: 'Would you like to know more',
 	    description: 'Do you want to know more'
 	}
