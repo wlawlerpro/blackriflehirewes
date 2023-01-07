ded51d6b5e0cf6d45c102899109b9e1b109b9ffd
Wes Lawler <weslawler@Wess-MacBook-Pro.local>
2023-01-06 22:09:17 -0500

    cleaned up product interface

--- src/app/skillfolder/product.ts
+++ src/app/skillfolder/product.ts
@@ -8,35 +8,4 @@ export interface Product {
 	description: string; 
     }
     
-    export const productss = 
-   [
-	{   id:1, 
-	    name: "Autographed picture of the Black Rifle Coffee CEO", 
-	    price: 69,
-	    productImage: 'https://lh5.googleusercontent.com/wtDyETV2XZBpRjpExK4oLAIoYcCP6AUTGL7Id0dycl8v3oFwaLR9Ei0FDqcxiVFdy2A=w2400',
-	    preview: 'Be you Be Andy',
-	    description: ` Are you feeling lonely? Are you sad? Fear not, because coming soon to your desk is
-	    your own autograph picture of the Black Rifle Coffee CEO, Andy Stumpf.
-	     Aimed at brightening your day, this stallion of a coffee roaster and wine sommelier, 
-	     will warm your heart as gaze longingly into his eyes. For the low low price of $69.69,
-	      you too can be on your way to stop being such a bitch. There’s no time to waste, 
-	      get your piece of this dick drawing davinci today! `
-
-	    
-	}, 
-	{   id: 2,
-	    name: "JT's Fat Clothes", 
-	    price: 42,
-	    productImage: 'https://lh6.googleusercontent.com/SggSfdd2rTlel_3s_04aUogHmVoH_TrvZB2d3cziNY2cxgWBdZiqthJIc-98Lawd1kc=w2400',  
-	    preview: 'Buy Now before he gets fat again',
-	    description: 'Are you NOT a fat phobic son of a bitch? Do you believe that there is more to life than being able  to look down and see your own dick? Then JTs is fat clothes is something that you need for your wardrobe. What once was donned by the Chris Farley-esque body that used to belong to Black Rifle Coffees Jarred Taylor, can now be yours for the low low price of $19.95Are you NOT a fat phobic son of a bitch? Do you believe that there is more to life than be able to look down and see your dick? ',
-	}, 
-	{   id:3,
-	    name: "Starship Troopers: The Logan Stark Reboot", 
-	    price: 20, 
-	    productImage: 'https://lh4.googleusercontent.com/u-cT25TQi2tXKzAloPPxWi-bMkDjDlZFhB0OhwPo_qssY1cPpx9zXQWLKm8a3AQAeQQ=w2400',
-	    preview: 'Would you like to know more',
-	    description: 'Do you want to know more'
-	}
-    ];
     
